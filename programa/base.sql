3cf6aae84515368219ff5c757d5c4b1da60b754b
Santy-git <santyjt4@gmail.com>
2024-03-12 17:58:13 -0300

    base de datos actualizada

--- programa/base.sql
+++ programa/base.sql
@@ -1,66 +1,90 @@
 -- Plantilla de la base de datos del programa
 
-CREATE TABLE IF NOT EXIST hotel (
+CREATE TABLE IF NOT EXISTS hotel (
     nombre varchar(50) not null,
     hab number(10) not null,
     pisos number(10) not null,
-    capac number(10) not null
+    capac number(10) not null -- total con todas las habitaciones ocupadas
 );
 
-CREATE TABLE IF NOT EXIST cochera (
-    codcochera int primary key,
-    piso int not null,
+CREATE TABLE IF NOT EXISTS cochera (
+    codCochera int primary key,
     estado varchar(50) not null
 );
 
+CREATE TABLE IF NOT EXISTS login(
+    codLog int primary key,
+    nivel varchar(20) not null
+);
+
 CREATE TABLE IF NOT EXIST empleado (
-    codempl int primary key,
+    codEmpleado int primary key,
     nombre varchar(100) not null,
     dni varchar(20) not null,
     email varchar(100) not null,
     telefono varchar(20) not null,
-    puesto varchar(100) not null
+    puesto varchar(100) not null,
+    codLog int not null,
+    foreign key (codLog) references login(codLog)
 );
 
-CREATE TABLE IF NOT EXIST cliente (
-    codcliente int primary key,
+CREATE TABLE IF NOT EXISTS cliente (
+    codCliente int primary key,
     nombre varchar(100) not null,
     dni varchar(20) not null,
     email varchar(100),
     descr varchar(255)
 );
 
-CREATE TABLE IF NOT EXIST habitacion (
-    codhab int primary key,
-    piso int not null,
-    cant int not null,
-    estado varchar(50) not null,
-    descr varchar(255),
-    costo decimal(10, 2)
+CREATE TABLE IF NOT EXISTS habitacion (
+    codHab int primary key,
+    piso number(10) not null,
+    camaMatr number(10) not null,
+    camaInd number(10) not null
 );
 
-CREATE TABLE IF NOT EXIST reserva (
-    codreserva int primary key,
-    codhab int not null,
-    codcliente int not null,
-    codempleado int not null,
-    codcochera int,
+CREATE TABLE IF NOT EXISTS reserva (
+    codReserva int primary key,
+    codReshab int not null,
+    codCliente int not null,
+    codEmpleado int not null,
+    codRescochera int,
     cantidad int not null,
-    fechaingreso date not null,
-    fechaegreso date not null,
-    costoadicional decimal(10, 2),
+    fechaIngreso date not null,
+    fechaEgreso date not null,
+    fechaReserva date not null,
     descr varchar(255),
-    foreign key (codhab) references habitacion(codhab),
-    foreign key (codcliente) references cliente(codcliente),
-    foreign key (codempleado) references empleado(codempl),
-    foreign key (codcochera) references cochera(codcochera)
+    foreign key (codHab) references habitacion(codHab),
+    foreign key (codCliente) references cliente(codCliente),
+    foreign key (codEmpleado) references empleado(codEmpleado),
+    foreign key (codRescoch) references cochera(codRescoch)
 );
 
-CREATE TABLE IF NOT EXIST historial (
-    codhistorial int primary key,
-    codreserva int not null,
-    codempleado int not null,
+CREATE TABLE IF NOT EXISTS historial (
+    codHistorial int primary key,
+    codReserva int not null,
+    codEmpleado int not null,
     descr varchar(255),
-    foreign key (codreserva) references reserva(codreserva),
-    foreign key (codempleado) references empleado(codempl)
+    foreign key (codReserva) references reserva(codReserva),
+    foreign key (codEmpleado) references empleado(codEmpleado)
+);
+
+CREATE TABLE IF NOT EXISTS resHab(
+    codReshab int primary key,
+    codHab int not null,
+    codReserva int not null,
+    camaMatr number(10) not null,
+    camaInd number(10) not null,
+    costoHab decimal(10,2) not null,
+    foreign key (codHab) references habitacion(codHab),
+    foreign key (codReserva) references reserva(codReserva)
+);
+
+CREATE TABLE IF NOT EXISTS resHab(
+    codRescoch int primary key,
+    codReserva int not null,
+    codCochera int not null,
+    foreign key (codCochera) references cochera(codCochera),
+    foreign key (codReserva) references reserva(codReserva)
+
 );
