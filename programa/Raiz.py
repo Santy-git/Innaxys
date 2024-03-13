9c7b9fde8986d530d31a605ce1b92f71c7ce1ac0
ongyz34 <156031091+ongyz34@users.noreply.github.com>
2024-03-13 14:10:36 -0300

    Raiz formateo

---
+++ programa/Raiz.py
@@ -0,0 +1,33 @@
+import flet as ft
+
+def raiz(page: ft.Page):
+    page.window_resizable = False
+    page.window_width = 600
+    page.window_height = 700
+    def check_item_clicked(e):
+        e.control.checked = not e.control.checked
+        page.update()
+
+    page.appbar = ft.AppBar(
+        leading = ft.Image(src="logoMini.svg", width=100, height=100),
+        title = ft.Text("Axys"),
+        center_title=True,
+        bgcolor=ft.colors.SURFACE_VARIANT,
+        actions=[
+            ft.IconButton(ft.icons.WB_SUNNY_OUTLINED),
+            ft.IconButton(ft.icons.FILTER_3),
+            ft.PopupMenuButton(
+                items=[
+                    ft.PopupMenuItem(text="Item 1"),
+                    ft.PopupMenuItem(),  
+                    ft.PopupMenuItem(
+                        text="Checked item", checked=False, on_click=check_item_clicked
+                    ),
+                ]
+            ),
+        ],
+    )
+    page.add(ft.Text("Body!"))
+    
+    
+ft.app(target=raiz)
