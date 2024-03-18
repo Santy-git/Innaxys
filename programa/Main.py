343415b801582be6b2297e146b24d08ba7d51b61
k1 <k1000ocs508@gmail.com>
2024-03-17 21:35:31 -0300

    Version Funcional Camilo

--- programa/Main.py
+++ programa/Main.py
@@ -1,5 +1,5 @@
 import flet as f
-from flet import TextField, Checkbox, ElevateButton, Text, Row, Column
+from flet import TextField, Checkbox, ElevatedButton, Text, Row, Column
 from flet_core.control_event import ControlEvent
 
 # new
@@ -7,7 +7,7 @@ from flet_core.control_event import ControlEvent
 #El None solo se usa porque solo corremos esta funcion en el codigo
 def main(page: f.Page) -> None:
     page.title = "INNAXYS"
-    page.vertical_aligment = f.MainAxisAligment.Center
+    page.vertical_alignment = f.MainAxisAlignment.CENTER
     # Crear una manera de cambiar de modo :)
     page.theme_mode = f.ThemeMode.LIGHT
     page.window_width = 400
@@ -16,10 +16,22 @@ def main(page: f.Page) -> None:
 
     #Setup
 
-    User: TextField = TextField(label="User", text_align=f.TextAlign.LEFT, WHITH= 200)
-    Password: TextField = TextField(label="Password", text_align=f.TextAlign.LEFT, WHITH= 200, password = True)
+    # Convertir esto en un CHiP o en algo mas bonito
+    Theme: Checkbox = Checkbox(label="Modo")
+    User: TextField = TextField(label="User", text_align=f.TextAlign.LEFT, width= 200)
+    Password: TextField = TextField(label="Password", text_align=f.TextAlign.LEFT, width= 200, password = True)
     checkbox: Checkbox = Checkbox(label="I agree to stuff",value = False)
-    Button: ElevateButton = ElevateButton(text="Sign Up")
+    Button: ElevatedButton = ElevatedButton(text="Sign Up")
+
+    def theme(e:ControlEvent) -> None:
+        if page.theme_mode == f.ThemeMode.LIGHT:
+            page.theme_mode = f.ThemeMode.DARK
+        else:
+            page.theme_mode = f.ThemeMode.LIGHT
+
+        page.update()
+
+
 
     def validate(e: ControlEvent) -> None:
         if all([User.value,Password.value,checkbox.value]):
@@ -37,13 +49,16 @@ def main(page: f.Page) -> None:
         page.add(
             Row(
                 controls=[Text(value=f'Welcome: {User.value}',size = 20)],
-                aligment=f.MainAxisAligment.CENTER
+                alignment = f.MainAxisAlignment.CENTER
             )
         )
+    
+    Theme.on_change = theme
     checkbox.on_change = validate
     User.on_change = validate
     Password.on_change = validate
     Button.on_change = sudmit
+
     
 
     #Render bonito de la pagina skeree
@@ -51,15 +66,16 @@ def main(page: f.Page) -> None:
         Row(
             controls=[
                 Column(
-                    [User,
+                    [Theme,
+                     User,
                      Password,
                      checkbox,
                      Button]
                 )
             ],
-            aligment = f.MainAxisAligment.CENTER
+            alignment = f.MainAxisAlignment.CENTER
         )
     )
 
-    if __name__ == '__main__':
-        f.app(target=main)
+if __name__ == '__main__':
+    f.app(target=main)
