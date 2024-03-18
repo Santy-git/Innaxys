111a101f46e47c5ce0707572122b0419d3647be8
ongyz34 <156031091+ongyz34@users.noreply.github.com>
2024-03-18 08:21:28 -0300

    Tratando de arreglar lo de camilo.py

--- programa/Raiz.py
+++ programa/Raiz.py
@@ -25,7 +25,8 @@ colores2 = [
  '#101e19']
 
 import flet as ft
-
+from flet import TextField, Checkbox, ElevatedButton, Text, Row, Column
+from flet_core.control_event import ControlEvent
 def main(raiz: ft.Page):
     raiz.window_prevent_close = True 
     def OFF(e):    
@@ -38,6 +39,7 @@ def main(raiz: ft.Page):
     raiz.window_title_bar_hidden = True
     raiz.window_frameless = True
     raiz.window_resizable = False
+    raiz.padding = 50
 
     raiz.appbar = ft.AppBar(
         title=ft.Text("Axys"),
@@ -45,10 +47,68 @@ def main(raiz: ft.Page):
         bgcolor=colores2[8],
         actions=[
             ft.IconButton(ft.icons.EXIT_TO_APP_ROUNDED, on_click=OFF),
+            #aca boton
         ],
     )
     raiz.add()
-    
+    login = ft.Container(
+        bgcolor = colores[5],
+        width=900,
+        height=600,
+        alignment = (0, 0))
+        """content=Row(
+            controls=[Column(
+                    usuario = TextField(label="User", text_align=ft.TextAlign.LEFT, width= 200),
+                    contraseña = TextField(label="Password", text_align=ft.TextAlign.LEFT, width= 200, password = True),
+                    derechos = Checkbox(label="I agree to stuff",value = False),
+                    boton = ElevatedButton(text="Sign Up"))],
+        ))"""
+    raiz.add(login)
+    """def validate(e: ControlEvent) -> None:
+        if all([Usuario.value,contraseña.value,derechos.value]):
+            Button.disabled = False
+        else:
+            Button.disabled = True
+
+        raiz.update()"""
+
+    """def sudmit(e: ControlEvent) -> None:
+        print("User:",usuario.value)
+
+        raiz.clean()
+        raiz.add(
+            Row(
+                controls=[Text(value=f'Welcome: {User.value}',size = 20)],
+                alignment = f.MainAxisAlignment.CENTER
+            )
+        )
+        
+        Theme.on_change = theme
+        checkbox.on_change = validate
+        User.on_change = validate
+        Password.on_change = validate
+        Button.on_change = sudmit
+
+        
+
+        #Render bonito de la pagina skeree
+       raiz.add(
+            Row(
+                controls=[
+                    Column(
+                        [
+                        Usuario,
+                        contraseña,
+                        checkbox,
+                        Button]
+                    )
+                ],
+                alignment = ft.MainAxisAlignment.CENTER
+            )
+        )"""
+        
+
+        
 
 ft.app(target=main)
 
