4dae18f27900bebbfc0bf7e567e95f8c5439577e
SantiGarciaaa <santiagogarcia@iico.edu.ar>
2024-03-27 11:28:22 -0300

    barracontactos

--- pagina/app.js
+++ pagina/app.js
@@ -3,3 +3,15 @@ function scrollToSection(sectionId) {
     behavior: "smooth",
   });
 }
+
+document.getElementById("checkbox").addEventListener("change", function() {
+  var contactBar = document.getElementById("contactBar");
+  var overlay = document.getElementById("overlay");
+  if (this.checked) {
+    contactBar.style.right = "0"; // Desplazar la barra hacia la izquierda
+    overlay.style.display = "block"; // Mostrar el fondo oscuro
+  } else {
+    contactBar.style.right = "-300px"; // Ocultar la barra hacia la derecha
+    overlay.style.display = "none"; // Ocultar el fondo oscuro
+  }
+});
