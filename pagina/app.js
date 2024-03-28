9b070eb3101306d7e8226cd73544e2cf37b8626e
Santy-git <santyjt4@gmail.com>
2024-03-28 20:01:48 -0300

    contactos y compatibilidad

--- pagina/app.js
+++ pagina/app.js
@@ -4,14 +4,11 @@ function scrollToSection(sectionId) {
   });
 }
 
-document.getElementById("checkbox").addEventListener("change", function() {
-  var contactBar = document.getElementById("contactBar");
-  var overlay = document.getElementById("overlay");
-  if (this.checked) {
-    contactBar.style.right = "0"; // Desplazar la barra hacia la izquierda
-    overlay.style.display = "block"; // Mostrar el fondo oscuro
+function toggleContactMenu() {
+  var menu = document.getElementById("contact-menu");
+  if (menu.style.display === "none") {
+    menu.style.display = "block";
   } else {
-    contactBar.style.right = "-300px"; // Ocultar la barra hacia la derecha
-    overlay.style.display = "none"; // Ocultar el fondo oscuro
+    menu.style.display = "none";
   }
-});
+}
