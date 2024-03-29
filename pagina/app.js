0bf1ff736218a13e45ec06b5c77c8098a42d9438
SantiGarciaaa <163474677+SantiGarciaaa@users.noreply.github.com>
2024-03-29 19:44:52 -0300

    megaSUPERfacha

--- pagina/app.js
+++ pagina/app.js
@@ -6,10 +6,9 @@ function scrollToSection(sectionId) {
 
 document.getElementById("checkbox").addEventListener("change", function() {
   var contactBar = document.getElementById("contactBar");
-  var overlay = document.getElementById("overlay");
   if (this.checked) {
-    contactBar.style.right = "0"; // Desplazar la barra hacia la izquierda
+    contactBar.style.right = "120"; // Desplazar la barra hacia la izquierda
   } else {
-    contactBar.style.right = "-300px"; // Ocultar la barra hacia la derecha
+    contactBar.style.right = "-700px"; // Ocultar la barra hacia la derecha
   }
 });
