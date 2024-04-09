4c4cf5ed5d6360593334f2ef9bb82812bf7ccb39
ongyz34 <156031091+ongyz34@users.noreply.github.com>
2024-04-09 18:35:00 -0300

    nose

--- programa/RaizF.py
+++ programa/RaizF.py
@@ -34,24 +34,98 @@ colores2 = [
     '#1e362d',
     '#101e19']
 #....................................
-class program(ft.UserControl):
-    def __init__(self):
-        pass
 
-def raiz(page: ft.Page):
-    raiz.window_prevent_close = True
-    raiz.window_height = 700
-    raiz.window_width = 500
-    raiz.window_bgcolor = colores2[0]
-    raiz.bgcolor = ft.colors.TRANSPARENT
+class Plantilla:
+    def __init__(self,raiz):
+        self.raiz = raiz
+    def ej(self):
+        self.raiz.bgcolor = ft.colors.AMBER
+        self.raiz.update()
+
+    def OFF(self,e):    
+        global cual    
+        self.raiz.window_destroy()
+        cual = 2
+
+    def EnEs(self,e):
+        global cual
+        self.raiz.window_destroy()
+        cual = 1
+
+    def logear(self,e):
+        global Nivel
+        global z
+        z =  login(self.User.value,self.Password.value)
+        print("pera",z)
+        try:
+            if z[0]:
+                Nivel = int(z[1])
+
+        except TypeError: 
+            dlg = ft.AlertDialog(
+            title=ft.Text("Usuario Incorrecto"), on_dismiss=lambda e: print("Dialog dismissed!")
+            )
+            def open_dlg(e):
+                self.raiz.dialog = dlg
+                dlg.open = True
+                self.raiz.update()
+            open_dlg(e)
+
+    def appbar(self):
+        nombre = ft.Container(content=Text(
+            "Axys", color=colores[9],), bgcolor=colores[3], width=200, height=40, border_radius=ft.border_radius.all(10))
+        nombre.alignment = ft.alignment.center
+
+        self.raiz.appbar = ft.AppBar(
+            title=nombre,
+            center_title=True,
+            bgcolor=colores2[8],
+            actions=[
+                ft.IconButton(ft.icons.LANGUAGE,on_click=self.EnEs,icon_size=35,bgcolor=ft.colors.RED),
+                ft.IconButton(ft.icons.EXIT_TO_APP_ROUNDED,on_click=self.OFF, icon_size=35,),
+            ]
+
+        )
+        self.raiz.update()
+
+    def login(self):
+        self.User: TextField = TextField(label="User", text_align=ft.TextAlign.LEFT, width= 200)
+        self.Password: TextField = TextField(label="Password", text_align=ft.TextAlign.LEFT, width= 200, password = True, can_reveal_password=True)
+        Button: ElevatedButton = ElevatedButton(text="Sign Up")
+        
+        Filas_login = Row(
+            controls=[
+                Column(
+                    [
+                        self.User,
+                        self.Password,
+                        Button]
+                )
+            ],
+            alignment=ft.MainAxisAlignment.CENTER,
+        )
+
+        contenedor_login = ft.Container(content=Filas_login, 
+                height=623, width=500, bgcolor=colores[3], 
+                border_radius=ft.border_radius.all(10), 
+                padding=ft.padding.only(top=70),
+                )
+        self.raiz.add(contenedor_login)
+
+
+
+def main(raiz: ft.Page):
+    raiz.window_prevent_close = True 
+    raiz.bgcolor = ft.colors.BLUE_GREY_100
     raiz.window_frameless = True
     raiz.window_resizable = False
-    raiz.padding = 10
     raiz.window_full_screen = True
-    Plantilla = program()
+    raiz.update()
+    objeto = Plantilla(raiz)
+    objeto.ej()
+    z = objeto.appbar()
+    print(z)
+    objeto.login()
 
-    # add application's root control to the page
-    raiz.add(Plantilla)
 
-
-ft.app(target=raiz)
+ft.app(target=main)
