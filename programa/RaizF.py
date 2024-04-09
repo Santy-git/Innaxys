7682137aee013c4aacfbde0b1641640564a78c47
ongyz34 <156031091+ongyz34@users.noreply.github.com>
2024-04-09 09:21:42 -0300

    raizF

---
+++ programa/RaizF.py
@@ -0,0 +1,9 @@
+#....................Librerias....................
+from crearBase import * 
+import flet as ft
+from flet import TextField, Checkbox, ElevatedButton, Text, Row, Column
+from flet_core.control_event import ControlEvent
+from datetime import datetime
+from functools import partial
+import time
+#.................................................
